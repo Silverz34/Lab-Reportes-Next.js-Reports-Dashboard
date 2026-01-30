e8c27da5de42a4f0fbba95c94f4c9e423eca2da8
Silverz34 <pradogordillomichael@gmail.com>
2026-01-30 14:55:47 -0600

    agregar archivo de roles, usuario de solo lectura

---
+++ src/db/roles.sql
@@ -0,0 +1,24 @@
+--ROLES.SQL 
+
+--por si ya existe el usuario 
+DROP ROLE IF EXIST readUser;
+
+-- Crear usuario de la aplicación 
+CREATE ROLE readUser WITH 
+LOGIN 
+PASSWORD 'stack135'
+NOSUPERUSER
+NOCREATEDB
+NOCREATEROLE
+INHERIT;
+
+--Permiso basico de conexion 
+GRANT USAGE ON SCHEMA public TO readUser;
+
+--Minimos permisos a solo lectura
+GRANT SELECT ON reports_vw_1 TO readUser;
+GRANT SELECT ON reports_vw_2 TO readUser;
+GRANT SELECT ON reports_vw_3 TO readUser;
+GRANT SELECT ON reports_vw_4 TO readUser;
+GRANT SELECT ON reports_vw_5 TO readUser;
+
