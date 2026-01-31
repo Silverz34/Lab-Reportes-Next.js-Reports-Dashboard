b45108628ca00fb04bdcc19bb0942d56e7a12313
Silverz34 <pradogordillomichael@gmail.com>
2026-01-31 11:57:37 -0600

    navbar improvisado

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+export default function navbar(){
+ return(
+    <header className="w-full bg-blue-600 text-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
+            <div className="text-xl font-bold">
+               MiApp
+            </div>
+      </div>
+    </header>
+ );
+}
