2a2754fe5076dac4c55f86ad64bbeb28eade32bf
Silverz34 <pradogordillomichael@gmail.com>
2026-01-31 22:35:35 -0600

    logica de paginacion, no se si este bien

--- components/paginacion.tsx
+++ components/paginacion.tsx
@@ -7,6 +7,14 @@ interface paginacionProps{
 } 
 
 export default function Paginacion({totalPage, MoreData}:paginacionProps){
+    const router = useRouter();
+    const search = useSearchParams();
+    const navigation = (direction: 'antes' | 'despues') => {    
+        const params = new URLSearchParams(search.toString());
+        const newPage = direction === 'despues'? totalPage + 1 : totalPage -1;
+        params.set('page', newPage.toString());
+        router.push(`?${params.toString()}`); 
+    };
 
     return(
         <div className="flex justify-center mt-8 space-x-4"></div>
