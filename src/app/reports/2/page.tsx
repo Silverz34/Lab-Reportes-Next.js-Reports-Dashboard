8927f916ac4b37a272e87bee7baa4f0b722a5bc1
Silverz34 <pradogordillomichael@gmail.com>
2026-01-31 22:03:50 -0600

    cambiare las interfaces a una carpeta despues

--- src/app/reports/2/page.tsx
+++ src/app/reports/2/page.tsx
@@ -1,4 +1,19 @@
-export default function Report2Page() {
+import { db } from "../../../../lib/db";
+import Link from "next/link";
+
+interface Reporte2Row{
+    categoria_id: number;
+    cantidad_productos: number;
+    promedio_precio: number;
+    etiqueta_precio: string;
+}
+
+export default async function Report2Page() {
+    const result = await db.query('SELECT * FROM reports_vw_2;');
+    const data: Reporte2Row[] = result.rows;
+    const sumaPromedios = data.reduce((acc, curr) => acc + Number(curr.promedio_precio), 0);
+    const kpiPromedio = data.length > 0 ? (sumaPromedios / data.length).toFixed(2) : 0;
+    
  return( 
         <div>
             <h1>Report 2</h1>
