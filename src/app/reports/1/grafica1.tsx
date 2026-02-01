9e0ca84e710248019649920772d028437851e206
Silverz34 <pradogordillomichael@gmail.com>
2026-01-31 18:33:24 -0600

    usando graficas de recharts

---
+++ src/app/reports/1/grafica1.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+
+export default function Grafica1({data}:{ data: {estado_orden:string, ordenes: number, monto_total: number, porcentaje: number}[] }) {
+ return (
+   <div className="h-64 w-full bg-white p-4 rounded-lg shadow">
+      <h3 className="text-gray-500 text-sm font-medium mb-4">Órdenes por Estado</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="estado_orden" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="ordenes" fill="#4F46E5" radius={[4, 4, 0, 0]} />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+ );
+} 
--- src/app/reports/1/page.tsx
+++ src/app/reports/1/page.tsx
@@ -1,4 +1,17 @@
-export default function Report1Page() {
+import { db } from "../../../../lib/db";
+import Grafica1 from "./grafica1";
+
+interface Reporte1Row{
+    estado_orden: string;
+    ordenes: number;
+    monto_total: number;
+    porcentaje: number;
+  }
+
+export default async function Report1Page() {
+  const resultado = await db.query('SELECT * FROM reports_vw_1;');
+  const data: Reporte1Row[] = resultado.rows;
+
   return (
         <div className="justify-items-start m-12">
           <h1 className="text-3xl font-bold mb-2">Reporte de estatus de orden</h1>
